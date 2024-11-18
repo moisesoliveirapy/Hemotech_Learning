19acd76dc6f3ca2d1ab07eecdeaef88368882700
moisesoliveirapy <moisesoliveira0204@Gmail.com>
2024-11-18 12:25:11 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -10,12 +10,12 @@ if ($bd->exec($sql))
     echo "apagado";
 
 $sql = "CREATE TABLE IF NOT EXISTS materiais (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     titulo VARCHAR(255) NOT NULL,
     tipo VARCHAR(20) NOT NULL,
     categoria VARCHAR(255) NOT NULL,
     arquivo VARCHAR(255) NOT NULL
-    
+       
 )";
 
 if ($bd->exec($sql))
