5fded8ad7a79f72bc92e4492fe34beb42f2d04d8
moisesoliveirapy <moisesoliveira0204@Gmail.com>
2024-11-18 11:56:05 -0300

    Update conexao.php

--- repository/conexao.php
+++ repository/conexao.php
@@ -3,8 +3,18 @@ class conexao
 {
     public static function criar(): PDO
     {
+        $env = parse_ini_file('.env');
+        $databaseType = $env["databasetype"];
+        $database = $env["database"];
+        $server = $env["server"];
+        $pass = $env["pass"];
+        $user = $env["user"];
+
+        if($databaseType == 'mysql'){
+            $database = "host=$server;=ddbname=$database";
+        }
         // Caminho relativo para o banco de dados SQLite
-        return new PDO("sqlite:" . __DIR__ . "/../config/materiais.db");
+        return new PDO("$databaseType:" . __DIR__ . "$database", $user, $pass);
     }
 }
 
@@ -12,20 +22,3 @@ class conexao
 ?> 
 
 <?php
-
-/* class Conexao{
-    public static function criar ():PDO{
-            $env = parse_ini_file('.env');
-            $databaseType = $env["databasetype"];
-            $database = $env["database"];
-            $server = $env["server"];
-            $pass = $env["pass"];
-            $user = $env["user"];
-
-            if($databaseType === "mysql"){
-                $database = "host=$server;dbname=$database";
-            }
-
-           return new PDO("$databaseType:$database", $user, $pass);
-    }
-} */
