2d9ae42ee3bf031b7ad781f5cefbf03740ffacd2
moisesoliveirapy <moisesoliveira0204@Gmail.com>
2024-11-19 08:35:23 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
-<?php
+/* <?php
 
 
 
@@ -16,6 +16,35 @@ $sql = "CREATE TABLE IF NOT EXISTS materiais (
     categoria VARCHAR(255) NOT NULL,
     arquivo VARCHAR(255) NOT NULL
        
+)";
+
+if ($bd->exec($sql))
+    echo "Tabela materiais criada";
+else echo "Erro";
+
+
+?>
+
+ */
+
+<?php
+
+
+
+$bd = new SQLite3("config/materiais.db");
+
+$sql = "DROP TABLE IF EXISTS materiais";
+
+if ($bd->exec($sql))
+    echo "apagado";
+
+$sql = "CREATE TABLE IF NOT EXISTS materiais (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(255) NOT NULL,
+    tipo VARCHAR(20) NOT NULL,
+    categoria VARCHAR(255) NOT NULL,
+    arquivo VARCHAR(255) NOT NULL
+    
 )";
 
 if ($bd->exec($sql))
